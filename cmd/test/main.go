f0edf7d45b0e708888e54ebd234cef2ec50486c1
fruitbars <huiyanghu@gmail.com>
2024-04-09 14:09:03 +0800

    更新示例程序

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -25,11 +25,33 @@ func testSparkChatSimpleWithCallback(prompt string, callback func(messages.ChatM
 	log.Println("完整结果", resp.GetContent())
 }
 
+func testSparkChatWithCallback(prompt string, callback func(messages.ChatMessage) error) {
+	req := go_sparkapi.SparkChatRequest{
+		Prompt:      prompt,
+		Topk:        4,
+		Temperature: 0.5,
+		Maxtokens:   8192,
+	}
+
+	resp, err := go_sparkapi.SparkChatWithCallback(req, callback)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	log.Println("完整结果", resp.GetContent())
+}
+
 func main() {
-	prompt := "天空是什么颜色的呢？"
+	prompt := "你好，大模型！"
 	//testSparkChatSimple(prompt)
 
 	testSparkChatSimpleWithCallback(prompt, func(msg messages.ChatMessage) error {
+		log.Println(msg.GetType(), msg.GetContent())
+		return nil
+	})
+
+	testSparkChatWithCallback(prompt, func(msg messages.ChatMessage) error {
 		log.Println(msg.GetType(), msg.GetContent())
 		return nil
 	})
